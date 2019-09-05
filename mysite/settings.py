3971416feb8ef72a07ababf6b19bd34e53cdfaea
Sweeneys <952328342@qq.com>
2019-09-05 15:39:53 +0800

    20190905

--- mysite/settings.py
+++ mysite/settings.py
@@ -141,7 +141,15 @@ STATIC_URL = '/static/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static')
+
+# Additional locations of static files
+STATICFILES_DIRS = [
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'static')
+]
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
