e0a47355c84fd2d3e1580b8fdc2901934698356b
Sweeneys <952328342@qq.com>
2019-10-08 18:33:29 +0800

    201909181833

--- apps/comment/admin.py
+++ apps/comment/admin.py
@@ -2,5 +2,10 @@
 from django.contrib import admin
 from .models import ArticleComment
 
-@admin.register()
 
+@admin.register(ArticleComment)
+class ArticleCommentAdmin(admin.ModelAdmin):
+    # 设置排序时间
+    date_hierarchy = 'create_at'
+    list_display = ('id', 'user', 'content', 'article', 'create_at')
+    ordering = '-id'
