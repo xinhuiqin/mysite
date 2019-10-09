ac3fb23684174102668df4adb59f80d7417d739d
Sweeneys <952328342@qq.com>
2019-10-09 17:03:11 +0800

    201910091703

--- apps/comment/templatetags/comment_tags.py
+++ apps/comment/templatetags/comment_tags.py
@@ -1,2 +1,14 @@
 # -*- coding:utf-8 -*-
+from django import template
 
+register = template.Library()
+
+
+@register.inclusion_tag('comment/tags/comment_list.html', takes_context=True)
+def get_comment_list(context, article):
+    """
+
+     评论列表模板.
+     1.article_comments为ArticleComments类中外键article的related_name
+    """
+    return article.article_comments.filter(parent=None)
