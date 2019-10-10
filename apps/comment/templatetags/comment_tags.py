49cb6d076176634a6669c87fb4a8b6f60fda2c69
Sweeneys <952328342@qq.com>
2019-10-10 11:48:05 +0800

    评论列表模板开发

--- apps/comment/templatetags/comment_tags.py
+++ apps/comment/templatetags/comment_tags.py
@@ -9,6 +9,8 @@ def get_comment_list(context, article):
     """
 
      评论列表模板.
-     1.article_comments为ArticleComments类中外键article的related_name
+     * 传递一篇文章，然后根据该文章加载评论。
+     * article_comments为ArticleComments类中外键article的related_name。
     """
-    return article.article_comments.filter(parent=None)
+    comment_list = article.article_comments.filter(parent=None)
+    return {'comment_list': comment_list}
