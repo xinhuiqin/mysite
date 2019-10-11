36fa0336f5a084acb7ceafe08146f9bee400e6af
Sweeneys <952328342@qq.com>
2019-10-11 14:11:23 +0800

    评论框

--- apps/comment/views.py
+++ apps/comment/views.py
@@ -1,15 +1,21 @@
 # -*- coding:utf-8 -*-
+import json
 from django.shortcuts import render
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
+from blog.models import Article
+
+
 
 user_model = settings.AUTH_USER_MODEL
+
 
 @login_required
 @require_POST
 def add_comment(request):
-    print(1111111)
     if request.is_ajax() and request.method == 'POST':
-        print(request.user)
+        article_id = request.POST.get('article_id')
+        article = Article.objects.get(id=article_id)
+        print(article_id)
